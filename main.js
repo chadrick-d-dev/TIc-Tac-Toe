4e91d35da839a0f119f0bf40d7c0258fe52620ac
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-08-04 17:30:44 -0600

    Modify naming of variables, revamp sqClick function, create updateTitles function

--- main.js
+++ main.js
@@ -17,4 +17,62 @@ var lftGrid = document.querySelector(".left-win-grid");
 var currentGame;
 
 window.onload = (currentGame = new Game());
-gameGrid.addEventListener("click", currentGame.sqClick);
+gameGrid.addEventListener("click", sqClick);
+
+function sqClick(event) {
+  var target = event.target;
+  if (target.innerText === "" && currentGame.turn === true && currentGame.gameWon === false) {
+    target.innerText = "🐯";
+    currentGame.trackGameBoard();
+    currentGame.turn = false;
+    gameAlert.innerText = `It's 🦁's turn`;
+    currentGame.winConditions();
+    currentGame.drawCondition();
+  } else if (target.innerText === "" && currentGame.turn === false && currentGame.gameWon === false) {
+    target.innerText = "🦁";
+    currentGame.trackGameBoard();
+    currentGame.turn = true;
+    gameAlert.innerText = `It's 🐯's Turn`;
+    currentGame.winConditions();
+    currentGame.drawCondition();
+  }
+}
+
+function resetBoard() {
+  this.turn = true;
+  this.gameWon = false;
+  gameAlert.innerText = `It's 🐯's Turn`
+  sq1.innerText = "";
+  sq2.innerText = "";
+  sq3.innerText = "";
+  sq4.innerText = "";
+  sq5.innerText = "";
+  sq6.innerText = "";
+  sq7.innerText = "";
+  sq8.innerText = "";
+  sq9.innerText = "";
+}
+function displayWin(player, side) {
+    player.retrieveWinsFromStorage(player.id);
+    var pBoards = player.boards;
+    side.innerHTML = "";
+      for (var i=0; i< pBoards.length; i++) {
+        side.insertAdjacentHTML('beforeend', `<section class="mini-game">
+            <div class="mini-square">${pBoards[i][0]}</div>
+            <div class="mini-square">${pBoards[i][1]}</div>
+            <div class="mini-square">${pBoards[i][2]}</div>
+            <div class="mini-square">${pBoards[i][3]}</div>
+            <div class="mini-square">${pBoards[i][4]}</div>
+            <div class="mini-square">${pBoards[i][5]}</div>
+            <div class="mini-square">${pBoards[i][6]}</div>
+            <div class="mini-square">${pBoards[i][7]}</div>
+            <div class="mini-square">${pBoards[i][8]}</div>
+          </section>` );
+      }
+  }
+
+  function updateTitles() {
+    lftHeader.innerText = currentGame.p1wins;
+    rtHeader.innerText = currentGame.p2wins;
+    gameAlert.innerText = currentGame.alert;
+  }
