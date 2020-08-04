ea37747b10deac220ff56182c0533455a554a14e
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-08-04 17:35:18 -0600

    Create new properties for game class, revamp game methods, modify and rename components of methods in game class

--- game.js
+++ game.js
@@ -5,77 +5,50 @@ class Game {
     this.player2 = new Player({ id: 'player2', token: '🐯' });
     this.turn = true;
     this.gameWon = false;
+    this.alert = "";
+    this.p1wins = "0 wins";
+    this.p2wins = "0 wins";
+    this.p1won = false;
+    this.p2won = false;
+    this.gameBoard = ["", "", "", "", "", "", "", "", ""];
   }
-  sqClick(event) {
-    var target = event.target;
-    if (target.innerText === "" && currentGame.turn === true && currentGame.gameWon === false) {
-      target.innerText = "🐯";
-      currentGame.turn = false;
-      gameAlert.innerText = `It's 🦁's turn`;
-      currentGame.gameDone(event);
-    } else if (target.innerText === "" && currentGame.turn === false && currentGame.gameWon === false) {
-      target.innerText = "🦁";
-      currentGame.turn = true;
-      gameAlert.innerText = `It's 🐯's Turn`;
-      currentGame.gameDone(event);
-    }
+  trackGameBoard() {
+    var boardValues = [sq1.innerText,  sq2.innerText, sq3.innerText, sq4.innerText, sq5.innerText, sq6.innerText, sq7.innerText, sq8.innerText, sq9.innerText];
+    this.gameBoard = boardValues;
   }
-  gameDone() {
-    if ((sq1.innerText === "🦁" && sq2.innerText === "🦁" && sq3.innerText === "🦁") || (sq4.innerText === "🦁" && sq5.innerText === "🦁" && sq6.innerText === "🦁") || (sq7.innerText === "🦁" && sq8.innerText === "🦁" && sq9.innerText === "🦁") || (sq1.innerText === "🦁" && sq4.innerText === "🦁" && sq7.innerText === "🦁") || (sq2.innerText === "🦁" && sq5.innerText === "🦁" && sq8.innerText === "🦁") || (sq3.innerText === "🦁" && sq6.innerText === "🦁" && sq9.innerText === "🦁") || (sq1.innerText === "🦁" && sq5.innerText === "🦁" && sq9.innerText === "🦁") || (sq3.innerText === "🦁" && sq5.innerText === "🦁" && sq7.innerText === "🦁")) {
+  winConditions() {
+    var board = this.gameBoard;
+    if ((board[0] === '🦁' && board[1] === '🦁' && board[2] === '🦁') || (board[3] === '🦁' && board[4] === '🦁' && board[5] === '🦁') || (board[6] === '🦁' && board[7] === '🦁' && board[8] === '🦁') || (board[0] === '🦁' && board[3] === '🦁' && board[6] === '🦁') || (board[1] === '🦁' && board[4] === '🦁' && board[7] === '🦁') || (board[2] === '🦁' && board[5] === '🦁' && board[8] === '🦁') || (board[0] === '🦁' && board[4] === '🦁' && board[8] === '🦁') || (board[2] === '🦁' && board[4] === '🦁' && board[6] === '🦁')) {
       this.gameWon = true;
+      // this.p1won = true;
       this.player1.wins += 1;
-      lftHeader.innerText = `${this.player1.wins} wins`;
-      gameAlert.innerText = `🦁 Wins!`;
-      this.player1.createWin();
-      currentGame.player1.saveWinsToStorage("player1");
-      this.displayWin(this.player1, lftGrid);
-      setTimeout(function() {currentGame.resetBoard();}, 1500);
-    } else if ((sq1.innerText === "🐯" && sq2.innerText === "🐯" && sq3.innerText === "🐯") || (sq4.innerText === "🐯" && sq5.innerText === "🐯" && sq6.innerText === "🐯") || (sq7.innerText === "🐯" && sq8.innerText === "🐯" && sq9.innerText === "🐯") || (sq1.innerText === "🐯" && sq4.innerText === "🐯" && sq7.innerText === "🐯") || (sq2.innerText === "🐯" && sq5.innerText === "🐯" && sq8.innerText === "🐯") || (sq3.innerText === "🐯" && sq6.innerText === "🐯" && sq9.innerText === "🐯") || (sq1.innerText === "🐯" && sq5.innerText === "🐯" && sq9.innerText === "🐯") || (sq3.innerText === "🐯" && sq5.innerText === "🐯" && sq7.innerText === "🐯")) {
+      this.player1.boards.push(this.gameBoard);
+      this.alert = `🦁 Wins!`;
+      this.p1wins = `${this.player1.wins} wins`;
+      this.player1.saveWinsToStorage("player1");
+      updateTitles();
+      displayWin(this.player1, lftGrid);
+      setTimeout(function() {resetBoard();}, 1500);
+    }else if ((board[0] === '🐯' && board[1] === '🐯' && board[2] === '🐯') || (board[3] === '🐯' && board[4] === '🐯' && board[5] === '🐯') || (board[6] === '🐯' && board[7] === '🐯' && board[8] === '🐯') || (board[0] === '🐯' && board[3] === '🐯' && board[6] === '🐯') || (board[1] === '🐯' && board[4] === '🐯' && board[7] === '🐯') || (board[2] === '🐯' && board[5] === '🐯' && board[8] === '🐯') || (board[0] === '🐯' && board[4] === '🐯' && board[8] === '🐯') || (board[2] === '🐯' && board[4] === '🐯' && board[6] === '🐯')) {
       this.gameWon = true;
       this.player2.wins += 1;
-      rtHeader.innerText = `${this.player2.wins} wins`;
-      gameAlert.innerText = `🐯 Wins!`;
-      this.player2.createWin();
+      this.player2.boards.push(this.gameBoard);
+      this.alert = `🐯 Wins!`;
+      this.p2wins = `${this.player2.wins} wins`;
       this.player2.saveWinsToStorage("player2");
-      this.displayWin(this.player2, rtGrid);
-      setTimeout(function() {currentGame.resetBoard();}, 1500);
-    }else if ((sq1.innerText === "🦁" || sq1.innerText === "🐯") && (sq2.innerText === "🦁" || sq2.innerText === "🐯") && (sq3.innerText === "🦁" || sq3.innerText === "🐯") && (sq4.innerText === "🦁" || sq4.innerText === "🐯") && (sq5.innerText === "🦁" || sq5.innerText === "🐯") && (sq6.innerText === "🦁" || sq6.innerText === "🐯") && (sq7.innerText === "🦁" || sq7.innerText === "🐯") && (sq8.innerText === "🦁" || sq8.innerText === "🐯") && (sq9.innerText === "🦁" || sq9.innerText === "🐯")) {
+      displayWin(this.player2, rtGrid);
+      updateTitles();
+      setTimeout(function() {resetBoard();}, 1500);
+    }
+  }
+  drawCondition() {
+    var board = this.gameBoard;
+    if ((board[0] === "🦁" || board[0] === "🐯") && (board[1] === "🦁" || board[1] === "🐯") && (board[2] === "🦁" || board[2] === "🐯") && (board[3] === "🦁" || board[3] === "🐯") && (board[4] === "🦁" || board[4] === "🐯") && (board[5] === "🦁" || board[5] === "🐯") && (board[6] === "🦁" || board[6] === "🐯") && (board[7] === "🦁" || board[7] === "🐯") && (board[8] === "🦁" || board[8] === "🐯")) {
       this.gameWon = true;
-      gameAlert.innerText = `It's a draw!`;
-      setTimeout(function() {currentGame.resetBoard();}, 1500);
+      this.alert = `It's a draw!`;
+      updateTitles();
+      setTimeout(function() {resetBoard();}, 1500);
     }
   }
-  resetBoard() {
-    this.turn = true;
-    this.gameWon = false;
-    gameAlert.innerText = `It's 🐯's Turn`
-    sq1.innerText = "";
-    sq2.innerText = "";
-    sq3.innerText = "";
-    sq4.innerText = "";
-    sq5.innerText = "";
-    sq6.innerText = "";
-    sq7.innerText = "";
-    sq8.innerText = "";
-    sq9.innerText = "";
-  }
-  displayWin(player, side) {
-      player.retrieveWinsFromStorage(player.id);
-      var pArray = player.boardArray;
-      console.log(player);
-      side.innerHTML = "";
-        for (var i=0; i< pArray.length; i++) {
-          side.insertAdjacentHTML('beforeend', `<section class="mini-game">
-              <div class="mini-square">${pArray[i][0]}</div>
-              <div class="mini-square">${pArray[i][1]}</div>
-              <div class="mini-square">${pArray[i][2]}</div>
-              <div class="mini-square">${pArray[i][3]}</div>
-              <div class="mini-square">${pArray[i][4]}</div>
-              <div class="mini-square">${pArray[i][5]}</div>
-              <div class="mini-square">${pArray[i][6]}</div>
-              <div class="mini-square">${pArray[i][7]}</div>
-              <div class="mini-square">${pArray[i][8]}</div>
-            </section>` );
-        }
-    }
+
 }
